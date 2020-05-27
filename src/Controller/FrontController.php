216281e9d4cb84262ac342fdd471de6406c4a008
Redcroun <64252086+Redcroun@users.noreply.github.com>
2020-05-27 09:25:51 +0200

    Page login

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    /**
+     * @Route("/", name="front")
+     */
+    public function index()
+    {
+        return $this->render('front/index.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+}
